d264c16e16ae5aa52fa0f75de4b6d8d544d16217
piro161616 <piro32603@gmail.com>
2024-09-06 23:38:17 +0900

    宝探しゲームのデータ一式です。

---
+++ src/main/java/plugin/treasurehunt/data/ExecutingPlayer.java
@@ -0,0 +1,42 @@
+package plugin.treasurehunt.data;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * ゲームを実行する際のスコア情報を扱うオブジェクト
+ * プレイヤー名、点数、日時の情報を持つ。
+ */
+@Getter
+@Setter
+public class ExecutingPlayer {
+  private String playerName;
+  private int score;
+  private Instant startTime;
+
+  // コンストラクタで初期値を設定
+  public ExecutingPlayer(String playerName, int score, Instant startTime) {
+    this.playerName = playerName;
+    this.score = score;
+    this.startTime = startTime;
+  }
+
+  private static List<ExecutingPlayer> executingPlayerList = new ArrayList<>();
+
+  public static void addPlayerScore(ExecutingPlayer executingPlayer) {
+    executingPlayerList.add(executingPlayer);
+  }
+
+  public static List<ExecutingPlayer> selectList() {
+    return new ArrayList<>(executingPlayerList);
+  }
+
+  // ゲーム時間とスコアをリセット
+  public void resetScore() {
+    this.score = 0;
+  }
+
+}
